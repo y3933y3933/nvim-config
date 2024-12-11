bb34b85d8c4253e81c79216da1ce0c19eddf2f7a
Joanne Lin <y3933y3933@gmail.com>
2024-12-11 23:48:33 +0800

    feat: add ts, eslint, prettier lsp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,6 +6,8 @@ return {
 			null_ls.setup({
 				sources = {
 					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.diagnostics.eslint_ls
 				},
 			})
 		end,
